208ac9e2057d7a4bd8d538c895c6ffd8dd79c457
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-01-02 14:48:46 +0100

    simple app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
+  Input,
+  Heading,
+  Card,
+  Image,
+  Stack,
+  CardBody,
+  Text,
+  CardFooter,
+  HStack,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { HiLocationMarker } from 'react-icons/hi';
+
+const api = {
+  key: 'e8be4b0ebc699ed60d6d0d33671e1f23',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = e => {
+    if (e.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  };
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Grid minH="20vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={3}>
+          <Heading size={'lg'}>Weather App</Heading>
+          <Input
+            maxW={'80vh'}
+            placeholder="Search location"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+          {typeof weather.main != 'undefined' ? (
+            <div>
+              <Card
+                direction={{ base: 'column', sm: 'row' }}
+                overflow="hidden"
+                variant="outline"
+                p={0}
+              >
+                <Image
+                  objectFit="contain"
+                  maxW={{ base: '100%', sm: '200px' }}
+                  src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt="Weather Icon"
+                />
+
+                <Stack>
+                  <CardBody textAlign="right">
+                    <Heading size="xl">
+                      {Math.round(weather.main.temp)}°C
+                    </Heading>
+                    <Text py="1" fontSize={'lg'}>
+                      {weather.weather[0].main}
+                    </Text>
+                  </CardBody>
+
+                  <CardFooter>
+                    <HStack>
+                      <HiLocationMarker />
+                      <Text fontSize={'md'}>{weather.name}</Text>
+                    </HStack>
+                  </CardFooter>
+                </Stack>
+              </Card>
+            </div>
+          ) : (
+            ''
+          )}
+        </VStack>
+      </Grid>
     </ChakraProvider>
   );
 }
